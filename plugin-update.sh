d903ddef07f13f5816bcce4fa1c70e19785c90e7
Me 200378 <200378@nearwood.nfshost.com>
2014-11-20 21:31:18 +0000

    added plugin-update.sh script (that's currently just comments). Updating update.sh to remove Hello Dolly plugin.

---
+++ plugin-update.sh
@@ -0,0 +1,19 @@
+#Hm, how to do this.
+#Need to get list from dir structure
+#Does that always match up to URL for update zip?
+#Seems to, minux version
+#
+#akismet                         index.php
+#captcha                         syntaxhighlighter
+#google-analytics-for-wordpress
+#
+#https://downloads.wordpress.org/plugin/akismet.3.0.3.zip
+#https://downloads.wordpress.org/plugin/captcha.4.0.6.zip
+#https://downloads.wordpress.org/plugin/google-analytics-for-wordpress.5.1.2.zip
+#
+#cd wp-content/plugins/
+#foreach plugin name
+#wget akismet-x.y.z.zip
+#rm -rf akismet/
+#unzip -oq akismet.zip
+#rm akismet.zip
